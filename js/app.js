cbfd9f39d11c67cdb45374ad34e44de6392e055c
Marc Qualie <marc@marcqualie.com>
2013-12-22 03:09:16 +0000

    Added twitter and disqus javascripts

--- js/app.js
+++ js/app.js
@@ -22,3 +22,18 @@ $(function() {
     template: '#search-results-template'
   });
 });
+
+// Disqus
+var disqus_shortname = 'marcqualie';
+$(function() {
+  var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+  dsq.src = '//' + disqus_shortname + '.disqus.com/' + ($('#disqus_thread').length > 0 ? 'embed' : 'count') + '.js';
+  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+});
+
+// Twitter
+$(function () {
+  if ($('.twitter-timeline').length > 0) {
+    !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+  }
+});
