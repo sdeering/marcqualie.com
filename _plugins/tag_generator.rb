a511f46cdd8e13ac3983475e4cf61989f96cc2a2
Marc Qualie <marc@marcqualie.com>
2014-01-08 13:35:33 +0000

    Allow for custom canonical urls on a per page basis

--- _plugins/tag_generator.rb
+++ _plugins/tag_generator.rb
@@ -8,6 +8,7 @@ module Jekyll
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'tag.html')
       self.data['tag'] = tag
+      self.data['canonical'] = "/tag/#{tag.downcase.gsub(' ', '-')}"
       tag_title_prefix = site.config['tag_title_prefix'] || 'Posts Tagged &ldquo;'
       tag_title_suffix = site.config['tag_title_suffix'] || '&rdquo;'
       self.data['title'] = "#{tag_title_prefix}#{tag}#{tag_title_suffix}"
